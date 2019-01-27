2caef6da58e54c901d2c02a5ce32cd3614a9bc76
Apfel <32610623+Apfel@users.noreply.github.com>
2019-01-27 16:10:39 +0100

    :weary:

--- structs.go
+++ structs.go
@@ -32,33 +32,27 @@ type Config struct {
 // RSS holds our RSS data in a simple struct.
 type RSS struct {
 	XMLName xml.Name `xml:"rss"`
-	Channel Channel  `xml:"channel"`
-}
 
-// Channel defines the RSS data for the channel itself.
-type Channel struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	Description string `xml:"description"`
-	Language    string `xml:"language"`
-	Copyright   string `xml:"copyright"`
-	Image       Image  `xml:"image"`
-	Item        Item   `xml:"item"`
-}
+	Channel struct {
+		Title       string `xml:"title"`
+		Link        string `xml:"link"`
+		Description string `xml:"description"`
+		Language    string `xml:"language"`
+		Copyright   string `xml:"copyright"`
 
-// Image gives the Channel a logo.
-type Image struct {
-	URL   string `xml:"url"`
-	Title string `xml:"title"`
-}
+		Image struct {
+			URL   string `xml:"url"`
+			Title string `xml:"title"`
+		} `xml:"image"`
 
-// Item is the actual data for our Channel.
-type Item struct {
-	Title     string `xml:"title"`
-	Link      string `xml:"link"`
-	PubDate   string `xml:"pubdate"`
-	Enclosure struct {
-		URL  string `xml:"url,attr"`
-		Type string `xml:"type,attr"`
-	} `xml:"enclosure"`
+		Item struct {
+			Title     string `xml:"title"`
+			Link      string `xml:"link"`
+			PubDate   string `xml:"pubdate"`
+			Enclosure struct {
+				URL  string `xml:"url,attr"`
+				Type string `xml:"type,attr"`
+			} `xml:"enclosure"`
+		} `xml:"item"`
+	} `xml:"channel"`
 }
